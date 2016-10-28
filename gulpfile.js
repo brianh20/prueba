0bedca1d9428e9683452b808a8360053002c910f
Nicolas Benito <nibenito@usdev.deloitte.com>
2016-10-28 14:13:41 -0500

    starter pack

--- gulpfile.js
+++ gulpfile.js
@@ -2,37 +2,35 @@ var gulp = require('gulp'),
 		jshint = require('gulp-jshint')
 		stylish = require('jshint-stylish'),
 		less = require('gulp-less'),
-		sourcemaps = require('gulp-sourcemaps'),
 		concat = require('gulp-concat'),
 		browserSync = require('browser-sync'),
 		reload = browserSync.reload;
-
+/*
 gulp.task('jshint', function(){
-	return gulp.src('./src/app/**/**/**/*.js')
+	return gulp.src('./src*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter(stylish))
-}) ;
+}) ;*/
 
 gulp.task('watch', function(){
-	gulp.watch('./src/styles/less/**/*.less', reload);
-	gulp.watch('./src/*.html').on('change', reload);
-	gulp.watch('./src/app/**/**/**/*').on('change', reload);
-	gulp.watch('./src/pages/**/*').on('change', reload);
+	gulp.watch('/*.js', reload);
+	gulp.watch('/*.html', reload);
 });
 
+/*
 gulp.task('less', function() {
-    return gulp.src('./src/styles/less/**/*.less')
+    return gulp.src('./srcless')
 	    .pipe(sourcemaps.init())
 	    .pipe(less())
 	    .pipe(concat('bundle.css'))
 	    .pipe(sourcemaps.write('.'))
 	    .pipe(gulp.dest('./src/styles/css'));
-});
+});*/
 
-gulp.task('serve', ['less'], function(){
+gulp.task('serve', function(){
 	browserSync({
 		server:{
-			baseDir: ['./src'],
+			baseDir: ['./'],
       routes : {
         '/bower_components' : 'bower_components'
       }
@@ -41,4 +39,4 @@ gulp.task('serve', ['less'], function(){
   gulp.start('watch');
 });
 
-gulp.task('default', ['serve', 'less', 'watch']);
+gulp.task('default', ['serve', 'watch']);
