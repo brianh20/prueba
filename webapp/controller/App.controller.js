c59c63b69e1f8eb5549463e3788273a4ef29ca07
Brian Hume <brian.hume@globant.com>
2016-11-03 12:08:34 -0300

    Packaged as a component

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,14 +1,17 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast"
+   "sap/m/MessageToast",
    //these are the required modules to inject
-], function (Controller, MessageToast) {
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
    "use strict";
    return Controller.extend("sap.ui.demo.wt.controller.App", {
-      onShowHello : function () {
-         // show a native JavaScript alert
-         alert("this is the button controller in action!");
-         MessageToast.show("This is the injected Toast module");
-      }
+		onShowHello : function () {
+		 // read msg from i18n model
+         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+         // show message
+         MessageToast.show(sMsg);
+		}
    });
 });
